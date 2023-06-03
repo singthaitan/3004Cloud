3a31aafa88f71750e3673a5acdef2741eb2effff
PohYiJieNicholas <2101720@sit.singaporetech.edu.sg>
2023-06-03 19:39:34 +0800

    Login and Sign up page

--- ElectricityWebApp/app.py
+++ ElectricityWebApp/app.py
@@ -3,36 +3,68 @@ import mysql.connector
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect(url_for('login'))
+    
+@app.route("/login", methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    
+        if username == 'nicholas' and password == '12345':
+            return redirect(url_for('home'))
 
-# @app.route("/getdata",methods=['GET'])
-# def data():
-#     if flask.request.method == 'GET':
-       
-#             date = datetime.now()
-#             date_str = date.strftime("%d/%m/%Y %H:%M:%S")
-#             sensor = message[0]
-#             sensor_data = float(message[1])
+        else:
+            pass
 
-#             print(sensor +":"+ str(sensor_data))
-                      
+    return render_template('login.html')
+
+@app.route("/signup", methods = ['GET','POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password_repeat = request.form['password-repeat']
+        if(password == password_repeat):
+            print("Reach")
+            # mySQL.dbCursor().execute("INSERT INTO accounts (username,password) VALUES (%s,%s)", (username, password))
+            # mySQL.dbCommit()
+            return redirect(url_for('login'))
+        else:
+            print("Reach no")
+            return render_template('signup.html')
+
+    return render_template('signup.html')
+    
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+@app.route("/getdata",methods=['GET'])
+def data():
+    if request.method == 'GET':
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="admin",
+            password="password",
+            port="3307",
+            database="hougang_power"
+        )
+
+        cursor = mydb.cursor()
+        cursor.execute("SELECT * FROM Readings")
+
+        allResults = cursor.fetchall()
+
 	    
-#     return jsonify(sensor = sensor, sensor_data = sensor_data,date = date_str, date1 = date_str)
+    return jsonify(readings = allResults)
 
 
 if __name__ == '__main':
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="admin",
-        password="password",
-        port="3307",
-        database="hougang_power"
-    )
-
-    mycursor = mydb.cursor()
-
-    mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
     app.run()
