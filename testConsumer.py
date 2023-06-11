69fcfc901e3c6113e59bbb355e6f4658333e42cb
shawnwongsit <2101229@sit.singaporetech.edu.sg>
2023-06-11 21:45:22 +0800

    Consumer will insert received data into mongodb

--- testConsumer.py
+++ testConsumer.py
@@ -1,13 +1,22 @@
 from kafka import KafkaConsumer, TopicPartition
 import json
+import pymongo
 
-# Setup
+# Setup for kafka
 hostname = "localhost"
 port = "9092"
 topic_name = "test"
+group_id = "firstConsumer"
 
+# Setup for mongodb
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["household_db"]
+collection = mydb["electricity_usage"]
+
+# Create consumer
 consumer = KafkaConsumer(
     client_id = "client1",
+    group_id = group_id,
     bootstrap_servers = hostname + ":" + str(port),
     value_deserializer = lambda v: json.loads(v.decode('ascii')),
     key_deserializer = lambda v: json.loads(v.decode('ascii')),
@@ -15,19 +24,31 @@ consumer = KafkaConsumer(
 )
 
 # Subscribe to the topic "test"
-# consumer.subscribe(topics=[topic_name])
-# consumer.subscription()
+consumer.subscribe(topics=[topic_name])
+consumer.subscription()
 
 # Reading from a specific offset value
-consumer.assign([TopicPartition(topic_name, 0)])
-consumer.seek(TopicPartition(topic_name, 0), 0)
+# consumer.assign([TopicPartition(topic_name, 0)])
+# consumer.seek(TopicPartition(topic_name, 0), 0)
 
 # Printing received messages
 for message in consumer:
     value = message.value
-    name = value['name']
-    temp = value['temp']
-    print("The name is " + name + " The temp is " + str(temp))
+    type = value['household_type']
+    hour = value['hour']
+    electricity_consumption = value['electricity_consumption']
     print(str(message.partition) + ":" + str(message.offset) + ":" + " k=" + str(message.key)
-          + " v=" + str(message.value))
+          + " v=" + str(value))
+    
+    # Combine the household id into value variable
+    mongoData = value.copy()
+    mongoData.update(message.key)
+
+    # Insert into mongodb collection
+    insert_result = collection.insert_one(mongoData)
+    if(insert_result.acknowledged):
+        print("Successfully inserted into db" + "\n")
+    else:
+        print("Failed to store data in db" + "\n")
+
     
--- testProducer.py
+++ testProducer.py
@@ -1,22 +1,51 @@
 from kafka import KafkaProducer
 import json
+import random
+import threading
+import datetime
+
+# Household 1
+def household1_thread():
+    timestamp = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    
+    # Data to be sent to broker
+    producer.send(
+        topic_name,
+        key={"household_id":1},
+        value={"household_type": 4, "hour": str(timestamp), "electricity_consumption":format(random.uniform(2, 200), ".2f")}
+    )
+
+# Household 2
+def household2_thread():
+    timestamp = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # Data to be sent to broker
+    producer.send(
+        topic_name,
+        key={"household_id":2},
+        value={"household_type": 5, "hour": str(timestamp), "electricity_consumption":format(random.uniform(2, 200), ".2f")}
+    )
 
 # Setup
 hostname = "localhost"
 port = "9092"
 topic_name = "test"
 
+# Create producer
 producer = KafkaProducer(
     bootstrap_servers = hostname + ":" + str(port),
     value_serializer = lambda v: json.dumps(v).encode('ascii'),
     key_serializer = lambda v: json.dumps(v).encode('ascii')
 )
 
-# Sending data to topic "test"
-producer.send(
-    topic_name,
-    key={"id":1},
-    value={"name":"Shawn", "temp":36.7}
-)
+# Start both household threads and send them concurrently
+thread1 = threading.Thread(target=household1_thread)
+thread1.start()
+
+thread2 = threading.Thread(target=household2_thread)
+thread2.start()
+
+thread1.join()
+thread2.join()
 
 producer.flush()
