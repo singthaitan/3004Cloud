c4571e6dc50f7816fba29c8b1d34ef045efac318
PohYiJieNicholas <2101720@sit.singaporetech.edu.sg>
2023-06-12 21:57:22 +0800

    Main page and simple javascript

--- ElectricityWebApp/app.py
+++ ElectricityWebApp/app.py
@@ -1,7 +1,12 @@
 from flask import *
+from flask_session import Session
+
 import mysql.connector
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -16,6 +21,7 @@ def login():
         password = request.form['password']
     
         if username == 'nicholas' and password == '12345':
+            session['username'] = username
             return redirect(url_for('home'))
 
         else:
@@ -31,8 +37,9 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         password_repeat = request.form['password-repeat']
-        if(password == password_repeat):
-            print("Reach")
+
+
+        if(username != "" and password == password_repeat):
             # mySQL.dbCursor().execute("INSERT INTO accounts (username,password) VALUES (%s,%s)", (username, password))
             # mySQL.dbCommit()
             return redirect(url_for('login'))
@@ -44,27 +51,44 @@ def signup():
     
 @app.route("/home")
 def home():
-    return render_template('index.html')
+    username = session['username']
+    return render_template('index.html',username=username)
 
 @app.route("/getdata",methods=['GET'])
 def data():
     if request.method == 'GET':
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="admin",
-            password="password",
-            port="3307",
-            database="hougang_power"
-        )
+        # mydb = mysql.connector.connect(
+        #     host="localhost",
+        #     user="admin",
+        #     password="password",
+        #     port="3307",
+        #     database="hougang_power"
+        # )
 
-        cursor = mydb.cursor()
-        cursor.execute("SELECT * FROM Readings")
+        # cursor = mydb.cursor()
+        # cursor.execute("SELECT * FROM Readings")
 
-        allResults = cursor.fetchall()
+        # allResults = cursor.fetchall()
 
+        list = [
+            {'address':"block 121 pasir ris street 11",
+             'householdType': '3-Room',
+             'hour': '2023-06-12 ,14:00',
+             'electricity': 1.5},
+            {'address':"block 121 pasir ris street 11",
+             'householdType': '3-Room',
+             'hour': '2023-06-12 ,15:00',
+             'electricity': 1.6},
+             {'address':"block 121 pasir ris street 11",
+             'householdType': '3-Room',
+             'hour': '2023-06-12 ,15:00',
+             'electricity': 1.7}
+        ]
+
+        print(list)
 	    
-    return jsonify(readings = allResults)
+    return jsonify(list)
 
 
 if __name__ == '__main':
-    app.run()
+    app.run(debug=True)
