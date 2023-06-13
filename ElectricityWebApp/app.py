064a46d934caf8b1b22d88e7b4783e70b0029954
Shermaine Peh <2101573@sit.singaporetech.edu.sg>
2023-06-13 19:07:46 +0800

    Revert "Merge branch 'WebApp-(Account)' into WebApp"
    This reverts commit 4d2c89cc369905e958470d3c84b5635d381b607f, reversing
    changes made to c4571e6dc50f7816fba29c8b1d34ef045efac318.

--- ElectricityWebApp/app.py
+++ ElectricityWebApp/app.py
@@ -9,96 +9,66 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route("/")
-def index():    
+def index():
     return redirect(url_for('login'))
     
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods = ['GET','POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
+    
+        if username == 'nicholas' and password == '12345':
+            session['username'] = username
+            return redirect(url_for('home'))
 
-        # Query the database for the user with the given email
-        user = collection.find_one({"e-mail address": email})
-
-        if user:
-            # Check if the password matches
-            if check_password_hash(user['password'], password):
-                # Password matches, perform login
-                # ... your login logic ...
-                return "Login successful"  # Replace with your desired response
-
-        # Invalid email or password
-        return "Invalid email or password"  # Replace with your desired response
+        else:
+            pass
 
     return render_template('login.html')
 
-
-@app.route('/register', methods=['GET','POST'])
-def register():
+@app.route("/signup", methods = ['GET','POST'])
+def signup():
     if request.method == 'GET':
-        return render_template('register.html')
+        return render_template('signup.html')
     elif request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        address = request.form['address']
-        unit = request.form['unit']
-        postal = request.form['postal']
-        household_type = request.form['household_type']
-        household_size = request.form['household_size']
-        region = request.form['region']
+        password_repeat = request.form['password-repeat']
 
-        # Check if the username already exists in the database
-        if collection.find_one({"e-mail address": email}):
-            return "This e-mail address have already registered for WattWise, please login instead."
+
+        if(username != "" and password == password_repeat):
+            # mySQL.dbCursor().execute("INSERT INTO accounts (username,password) VALUES (%s,%s)", (username, password))
+            # mySQL.dbCommit()
+            return redirect(url_for('login'))
         else:
-            # Hash the password
-            hashed_password = generate_password_hash(password)
+            print("Reach no")
+            return render_template('signup.html')
 
-            # Create a new document for the user
-            user = {
-                "first name": first_name,
-                "last name": last_name,
-                "e-mail address": email,
-                "password": hashed_password,
-                "street address": address,
-                "unit number": unit,
-                "postal code": postal,
-                "household type": household_type,
-                "household size": household_size,
-                "region": region
-            }
-
-            # Insert the document into the collection
-            collection.insert_one(user)
-
-            # Redirect to the login page
-            return redirect('/login')
+    return render_template('signup.html')
     
 @app.route("/home")
 def home():
     username = session['username']
     return render_template('index.html',username=username)
 
-# @app.route("/getdata",methods=['GET'])
-# def data():
-#     if request.method == 'GET':
-#         mydb = mysql.connector.connect(
-#             host="localhost",
-#             user="admin",
-#             password="password",
-#             port="3307",
-#             database="hougang_power"
-#         )
+@app.route("/getdata",methods=['GET'])
+def data():
+    if request.method == 'GET':
+        # mydb = mysql.connector.connect(
+        #     host="localhost",
+        #     user="admin",
+        #     password="password",
+        #     port="3307",
+        #     database="hougang_power"
+        # )
 
-#         cursor = mydb.cursor()
-#         cursor.execute("SELECT * FROM Readings")
+        # cursor = mydb.cursor()
+        # cursor.execute("SELECT * FROM Readings")
 
-#         allResults = cursor.fetchall()
+        # allResults = cursor.fetchall()
 
         list = [
             {'address':"block 121 pasir ris street 11",
@@ -118,7 +88,6 @@ def home():
         print(list)
 	    
     return jsonify(list)
-#     return jsonify(readings = allResults)
 
 
 if __name__ == '__main':
