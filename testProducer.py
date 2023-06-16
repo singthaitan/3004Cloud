20647fc567c070bdbebc368d6540038e6c6c272b
shawnwongsit <2101229@sit.singaporetech.edu.sg>
2023-06-16 13:27:45 +0800

    Refactor code and sends different range based on time

--- testProducer.py
+++ testProducer.py
@@ -28,39 +28,66 @@ def getAllHouseholdID(household_type):
     listOfID = roomTypeDict[household_type]
     return listOfID
 
-# Household 1
-household1Count = 0
-def household1_thread():
-    global household1Count
-    listOfID = getAllHouseholdID("1 Room")
-    # Send data every 10 sec (Simulate sending of data every 1 hour)
-    threading.Timer(10.0, household1_thread).start()
-    timestamp = datetime.datetime.now().replace(minute=0, microsecond=0)
-    household1Count += 1
+def getRange(timestamp, householdType):
+    # rangeDict is for 1 room multiply by 20% for subsequent household types
+    rangeDict = {
+        "range1": [10,15], # 12am to 6am
+        "range2": [25,30], # 7am to 10am
+        "range3": [30,40], # 11am to 7pm
+        "range4": [35,55] # 8pm to 11pm
+    }
+
+    householdTypeMultiplier = 1
+    householdTypeNumber = householdType.split()[0]
+    for i in range(int(householdTypeNumber)):
+        householdTypeMultiplier *= 1.2
+
+    # print(householdTypeMultiplier)
+
+    # timestamp = datetime.datetime(2023, 6, 16, 20, 0, 0, 0)
+    hour = timestamp.hour
+    rangeType = None
+    if 0 <= hour <= 6:
+        rangeType = "range1"
+    elif 7 <= hour <= 10:
+        rangeType = "range2"
+    elif 11 <= hour <= 19:
+        rangeType = "range3"
+    elif 20 <= hour <= 23:
+        rangeType = "range4"
+    else:
+        print("Error in getting range type")
+
+    # print(rangeType)
+    return rangeDict[rangeType][0], rangeDict[rangeType][1]
+
+# Household thread
+def household_thread(householdType, householdCount, chosenID):
+
+    # print("Household1 Count: " + str(householdCount))
+    # print("Chosen ID: " + str(chosenID))
+    listOfID = getAllHouseholdID(householdType)
+    
+    timestamp = datetime.datetime.now().replace(minute=0, second=0, microsecond=0)
+    timestamp += datetime.timedelta(hours=householdCount)
+    householdCount += 1
+
+    rangeMin, rangeMax = getRange(timestamp, householdType)
+
+    # Reset chosenID
+    if chosenID >= len(listOfID):
+        chosenID = 0
 
     # Data to be sent to broker
     producer.send(
         topic_name,
-        key={"household_id":parse_json(listOfID[household1Count % 2])},
-        value={"timestamp": str(timestamp), "electricity_consumption":float(format(random.uniform(1.776, 393.632), ".2f"))}
+        key={"household_id":parse_json(listOfID[chosenID])},
+        value={"timestamp": str(timestamp), "electricity_consumption":float(format(random.uniform(rangeMin, rangeMax), ".2f"))}
     )
+    chosenID += 1
 
-# Household 2
-household2Count = 0
-def household2_thread():
-    global household2Count
-    listOfID = getAllHouseholdID("2 Room")
     # Send data every 10 sec (Simulate sending of data every 1 hour)
-    threading.Timer(10.0, household2_thread).start()
-    timestamp = datetime.datetime.now().replace(minute=0, microsecond=0)
-    household2Count += 1
-
-    # Data to be sent to broker
-    producer.send(
-        topic_name,
-        key={"household_id":parse_json(listOfID[household2Count % 2])},
-        value={"timestamp": str(timestamp), "electricity_consumption":float(format(random.uniform(2.1312, 472.3584), ".2f"))}
-    )
+    threading.Timer(10.0, household_thread, args=[householdType, householdCount, chosenID]).start()
 
 # Setup
 hostname = "localhost"
@@ -79,10 +106,14 @@ producer = KafkaProducer(
 )
 
 # Start both household threads and send them concurrently
-thread1 = threading.Thread(target=household1_thread)
+household1Count1 = 0
+chosenID1 = 0
+thread1 = threading.Thread(target=household_thread("3 Room", household1Count1, chosenID1))
 thread1.start()
 
-thread2 = threading.Thread(target=household2_thread)
+household1Count2 = 0
+chosenID2 = 0
+thread2 = threading.Thread(target=household_thread("2 Room", household1Count2, chosenID2))
 thread2.start()
 
 thread1.join()
