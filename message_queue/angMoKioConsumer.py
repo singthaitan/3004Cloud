3b0bea80282bf2d42d194287a07d371a2d9ba0b7
shawnwongsit <2101229@sit.singaporetech.edu.sg>
2023-06-19 15:29:17 +0800

    Updated message queue to send data to all regions

---
+++ message_queue/angMoKioConsumer.py
@@ -0,0 +1,56 @@
+from kafka import KafkaConsumer, TopicPartition
+import json
+import pymongo
+from bson import ObjectId
+
+# Setup for kafka
+hostname = "localhost"
+port = "9092"
+topic_name = "electricity_consumption"
+group_id = "angmokioConsumer"
+
+# Setup for mongodb
+
+# Local mongodb
+# myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+# mydb = myclient["household_db"]
+# collection = mydb["electricity_usage"]
+
+# Mongodb Atlas
+myclient = pymongo.MongoClient("mongodb+srv://shawn:shawn@app-cluster.zxcw8od.mongodb.net/")
+mydb = myclient["AngMoKio-Electric"]
+collection = mydb["query_elec"]
+
+
+# Create consumer
+consumer = KafkaConsumer(
+    client_id = "angmokio_client",
+    group_id = group_id,
+    bootstrap_servers = hostname + ":" + str(port),
+    value_deserializer = lambda v: json.loads(v.decode('ascii')),
+    key_deserializer = lambda v: json.loads(v.decode('ascii')),
+    max_poll_records = 10
+)
+
+consumer.assign([TopicPartition(topic_name, 0)])
+
+# Printing received messages
+for message in consumer:
+    value = message.value
+    hour = value['timestamp']
+    electricity_consumption = value['electricity_consumption']
+    print(str(message.partition) + ":" + str(message.offset) + ":" + " k=" + str(message.key['household_id']['$oid'])
+          + " v=" + str(value))
+    
+    # Combine the household id into value variable
+    mongoData = value.copy()
+    mongoData.update({"household_id": ObjectId(message.key['household_id']['$oid'])})
+
+    # Insert into mongodb collection
+    insert_result = collection.insert_one(mongoData)
+    if(insert_result.acknowledged):
+        print("Successfully inserted into db" + "\n")
+    else:
+        print("Failed to store data in db" + "\n")
+
+    
